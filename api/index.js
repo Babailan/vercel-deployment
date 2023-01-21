85e66f6725b47d73291c1541100cde77ed645034
babailanxx@gmail.com <babailanxx@gmail.com>
2023-01-21 19:20:26 +0800

    express api

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const app = require("express")();
+
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+module.exports = app;
